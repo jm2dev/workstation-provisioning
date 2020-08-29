4f459b1d8b0c7e31c105a5c78ca819f6796fbe96
Jose Miguel <jm@0pt1mates.com>
2020-08-29 10:43:24 +0100

    Browsers and spectrwm to use rofi.

--- roles/archlinux/files/dos-monitores.sh
+++ roles/archlinux/files/dos-monitores.sh
@@ -8,10 +8,10 @@ EXT="HDMI2"
 if (xrandr | grep "${EXT} disconnected"); then
     xrandr --output $IN --auto $EXT --off
     #feh  --bg-fill $HOME/Pictures/wallpapers/wolf-in-sheep-clothes.jpg
-    feh --bg-fill $HOME/Pictures/wallpapers/tank-blueprint-digital-art-hd-wallpaper-1920x1200-5052.jpg
+    feh --bg-fill $HOME/Pictures/wallpapers/ships.jpg
 else
     xrandr --output $IN --auto --output $EXT --auto --right-of $IN
     #feh  --bg-fill $HOME/Pictures/wallpapers/BlackMarble_2016_01deg.jpg $HOME/Pictures/wallpapers/ships.jpg
-    feh --bg-fill $HOME/Pictures/wallpapers/tank-blueprint-digital-art-hd-wallpaper-1920x1200-5052.jpg
+    feh --bg-fill $HOME/Pictures/wallpapers/debian-lines.png $HOME/Pictures/wallpapers/ships.jpg
 fi
 
